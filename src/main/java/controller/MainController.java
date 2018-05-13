531955fa13d2cfd31a5be2d684f2fbd9ae1c8f08
Wiktor Florencki <wiktor.florencki@gmail.com>
2018-05-13 19:43:56 +0100

    Updated main controller to enable load files.

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,11 +1,38 @@
 package controller;
 
+import exception.LoaderException;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
+import service.MainService;
+
+import java.io.File;
+import java.util.List;
 
 public class MainController {
     @FXML
     private TextArea aTextArea;
 
 
+    private MainService service;
+
+    public MainController() {
+        this.service = new MainService();
+    }
+
+    public void loadSoundFiles(MouseEvent mouseEvent) {
+        try {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Open Resource File");
+            fileChooser.setInitialDirectory(new File("C:/Users/Donski/Desktop/thesis/genres"));
+            List<File> selectedFile = fileChooser.showOpenMultipleDialog(null);
+            selectedFile.forEach(f -> System.out.println(f.getPath()));
+            service.createTrainingSet(selectedFile);
+        } catch (LoaderException e) {
+            e.printStackTrace();
+        }
+        double[][] a = service.getTrainingSet();
+        System.out.println("TEST");
+    }
 }
